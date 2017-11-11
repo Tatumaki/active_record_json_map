2606c831966058bc01beba9e088361eed37e4d89
Shin Matsudaira <shin.matsudaira@techouse.jp>
2017-11-11 13:52:50 +0900

    add core code

--- active_record_json_map.gemspec
+++ active_record_json_map.gemspec
@@ -24,5 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
 
-  spec.add_dependency "active_record", "~> 4.0.0"
+  spec.add_dependency "activerecord"
 end
--- lib/active_record_json_map.rb
+++ lib/active_record_json_map.rb
@@ -1,5 +1,43 @@
 require "active_record_json_map/version"
 
 module ActiveRecordJsonMap
-  # Your code goes here...
+  def self.included klass
+    klass.class_variable_set("@@format", klass.to_s.underscore)
+
+    if klass.ancestors.include? ActiveRecord::Base
+      const_get("#{klass.to_s}::ActiveRecord_Relation").class_eval do
+        def json_map key=nil, *args, **hash
+          self.map {|it| it.json key, *args, **hash}
+        rescue ArgumentError
+          self.map {|it| it.json key}
+        end
+      end
+
+      const_get("#{klass.to_s}::ActiveRecord_AssociationRelation").class_eval do
+        def json_map key=nil, *args, **hash
+          self.map {|it| it.json key, *args, **hash}
+        rescue ArgumentError
+          self.map {|it| it.json key}
+        end
+      end
+
+      const_get("#{klass.to_s}::ActiveRecord_Associations_CollectionProxy").class_eval do
+        def json_map key=nil, *args, **hash
+          self.map {|it| it.json key, *args, **hash}
+        rescue ArgumentError
+          self.map {|it| it.json key}
+        end
+      end
+    end
+  end
+
+  def json key=nil, *args, **hash
+    format!((key ? self.send(key, *args, **hash) : self.attributes), key: key)
+  end
+
+  def format! hash={}, key: nil, **other
+    hash.merge! other
+    hash[:format] ||= key ? "#{self.class.to_s.underscore}_#{key}" : nil || @format || self.class.class_variable_get("@@format")
+    return hash
+  end
 end
